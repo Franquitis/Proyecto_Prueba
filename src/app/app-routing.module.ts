c91d3118b47bf6fc13d395a9916794794f1ca172
jjoacoc <jjoaco.cofre@gmail.com>
2024-04-23 10:53:24 -0300

    creamos laprimera interfaz, creamos el componente card

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './modules/inicio/components/inicio/inicio.component';
 
 const routes: Routes = [
+  //ruta por defecto en la inicializacion
   {
     path:"",component:InicioComponent
   },
+  //ruta que nos vincula el modulo de inicio y todo su contenido
   {
     path:"",loadChildren:()=>import('./modules/inicio/inicio.module').then(m=>m.InicioModule)
   }
