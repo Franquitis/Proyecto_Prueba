68e039a32b51b2845373b81ebd47d0ab82d50d5d
jjoacoc <jjoaco.cofre@gmail.com>
2024-06-05 10:21:59 -0300

    realizamos la funcion de limpiar los inputs y realizamos la actividad de inicio sesion

--- src/app/modules/autentificacion/pages/iniciosesion/iniciosesion.component.ts
+++ src/app/modules/autentificacion/pages/iniciosesion/iniciosesion.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Usuario } from 'src/app/models/usuario';
 
 @Component({
   selector: 'app-iniciosesion',
@@ -6,5 +7,63 @@ import { Component } from '@angular/core';
   styleUrls: ['./iniciosesion.component.css']
 })
 export class IniciosesionComponent {
+  hide = true;
+  public perfil: Usuario[];
 
+  //en el siguiente arreglo importamos los datos del usuario
+  constructor() {
+    this.perfil = [
+      {
+        uid: '',
+        email: 'franco@gmail.com',
+        password: 'fortnite12',
+        nombre: 'franco',
+        apellido: 'quiroga',
+        rol: 'vist'
+      }
+    ]
+  }
+
+  perfiles: Usuario = {
+    uid: '',
+    nombre: '',
+    apellido: '',
+    email: '',
+    rol: '',
+    password: ''
+  }
+
+  coleccionPerfiles: Usuario[] = [];
+
+  comparador() {
+    //esta constante lo que hace es resguardar la informacion que ingrese el usuario
+    const credenciales = {
+      uid: this.perfiles.uid,
+      nombre: this.perfiles.nombre,
+      apellido: this.perfiles.apellido,
+      email: this.perfiles.email,
+      rol: this.perfiles.rol,
+      password: this.perfiles.password,
+    }
+ //creo un for que recorra el arreglo con los usuarios subidos
+    for (let i = 0; i <= this.perfil.length ; i++) {
+      //creamos la constante 
+      const orden = this.perfil[i]
+      //creamos un if el cual compare los datos
+      if (orden.uid === credenciales.uid && orden.nombre === credenciales.nombre && orden.apellido === credenciales.apellido && orden.email === credenciales.email && orden.rol === credenciales.rol && orden.password === credenciales.password) {
+        alert("Inicio sesion correctamente")
+        
+      }else{
+        alert("Erorr, los datos no son correctos")
+        
+      }
+      
+    }
+
+    this.coleccionPerfiles.push(credenciales)
+
+    console.log(credenciales);
+    console.log(this.coleccionPerfiles)
+  }
 }
+
--- src/app/modules/autentificacion/pages/registros/registros.component.ts
+++ src/app/modules/autentificacion/pages/registros/registros.component.ts
@@ -8,37 +8,51 @@ import { Usuario } from 'src/app/models/usuario';
 })
 export class RegistrosComponent {
   //input de la contraseña para ver los caracteres o no
-hide = true;
+  hide = true;
 
-//IMPORTAR INTERFAZ DE USUARIO
-usuarios: Usuario = {
-  uid: '', // -> inicializamos con comillas simples porque es STRING
-  nombre: '',
-  apellido: '',
-  email:'',
-  rol: '',
-  password: ''
-}
-//CREAMOS COLECCION DE USUARIOS, TIPO "USUARIO" PARA ARRAYS
-coleccionUsuarios: Usuario[]=[];
+  //IMPORTAR INTERFAZ DE USUARIO
+  usuarios: Usuario = {
+    uid: '', // -> inicializamos con comillas simples porque es STRING
+    nombre: '',
+    apellido: '',
+    email: '',
+    rol: '',
+    password: ''
+  }
+  //CREAMOS COLECCION DE USUARIOS, TIPO "USUARIO" PARA ARRAYS
+  coleccionUsuarios: Usuario[] = [];
 
-//FUNCION PARA EL REGISTROS DE NUEVOS USUARIOS
-registrar(){
-  //constante de credenciales, va a resguardar la informacion que ingrese ell usuario
-const credenciales = {
-  uid: this.usuarios.uid, // definimos al atributo de la interfaz con una variable local
-  nombre: this.usuarios.nombre,
-  apellido: this.usuarios.apellido,
-  email: this.usuarios.email,
-  rol: this.usuarios.rol,
-  password: this.usuarios.password
-}
+  //FUNCION PARA EL REGISTROS DE NUEVOS USUARIOS
+  registrar() {
+    //constante de credenciales, va a resguardar la informacion que ingrese ell usuario
+    const credenciales = {
+      uid: this.usuarios.uid, // definimos al atributo de la interfaz con una variable local
+      nombre: this.usuarios.nombre,
+      apellido: this.usuarios.apellido,
+      email: this.usuarios.email,
+      rol: this.usuarios.rol,
+      password: this.usuarios.password
+    }
 
-//enviamos la nueva infirmacion como un nuevo objeto a la coleccion de usuarios
-this.coleccionUsuarios.push(credenciales)
-
-//mostramos credenciales por consola
-console.log(credenciales);
-console.log(this.coleccionUsuarios);
-}
+    //enviamos la nueva informacion como un nuevo objeto a la coleccion de usuarios
+    this.coleccionUsuarios.push(credenciales)
+    this.cleaner()
+    //mostramos credenciales por consola
+    console.log(credenciales);
+    console.log(this.coleccionUsuarios);
+ 
+ 
+  }
+//Funcion para limpiar los inputs
+  cleaner() {
+    const input = {
+      uid: this.usuarios.uid = '',
+      nombre: this.usuarios.nombre = '',
+      apellido: this.usuarios.apellido = '',
+      email: this.usuarios.email = '',
+      rol: this.usuarios.rol = '',
+      password: this.usuarios.password = ''
+    }
+    alert("Se registro corectamente")
+  }
 }
