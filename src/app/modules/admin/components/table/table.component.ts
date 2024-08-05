d1834b96a41655451cf1669bd8561fe3e36a2fcc
agusfloress07 <agus07antonellaa@gmail.com>
2024-08-05 11:03:51 -0300

    creacion del metodo agregar producto desde el table ts y creacion de modal desde html

--- src/app/modules/admin/components/table/table.component.ts
+++ src/app/modules/admin/components/table/table.component.ts
@@ -10,22 +10,44 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./table.component.css']
 })
 export class TableComponent {
-// Creamos coleccion local de productos -> la definimos como array
-coleccionProductos:Producto[]=[];
+  // Creamos coleccion local de productos -> la definimos como array
+  coleccionProductos: Producto[] = [];
 
-//definimos formulario para los productos
+  //definimos formulario para los productos
 
-/*
-atributos alfanumericos (string) se inicializan con comillas simples
-atributos numericos (number) se inicializan con 0
-*/
-producto = new FormGroup({
-nombre:new FormControl('',Validators.required),
-precio:new FormControl(0,Validators.required),
-descripcion:new FormControl('',Validators.required),
-categoria:new FormControl('',Validators.required),
-imagen:new FormControl('',Validators.required),
-alt:new FormControl('',Validators.required),
+  /*
+  atributos alfanumericos (string) se inicializan con comillas simples
+  atributos numericos (number) se inicializan con 0
+  */
+  producto = new FormGroup({
+    nombre: new FormControl('', Validators.required),
+    precio: new FormControl(0, Validators.required),
+    descripcion: new FormControl('', Validators.required),
+    categoria: new FormControl('', Validators.required),
+    imagen: new FormControl('', Validators.required),
+    alt: new FormControl('', Validators.required),
 
-})
+  })
+
+  constructor(public servicioCrud: CrudService) { }
+  ngOninit(): void { }
+  async agregarProducto() {
+    if (this.producto.valid) {
+      let nuevoProducto: Producto = {
+        idProducto: '',
+        nombre: this.producto.value.nombre!,
+        precio: this.producto.value.precio!,
+        descripcion: this.producto.value.descripcion!,
+        categoria: this.producto.value.categoria!,
+        imagen: this.producto.value.imagen!,
+        alt: this.producto.value.alt!
+      }
+      await this.servicioCrud.crearProducto(nuevoProducto)
+        .then(producto => {
+          alert("Ha agregado un nuevo producto con exito!")
+        }).catch(error => {
+          alert("Ha ocurrido n error al cargar el producto")
+        })
+    }
+  }
 }
