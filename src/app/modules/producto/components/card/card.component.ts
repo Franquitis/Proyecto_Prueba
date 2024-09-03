42dbbabf686de2c94c2656b9214a18168e0ff94b
agusfloress07 <agus07antonellaa@gmail.com>
2024-09-03 11:05:21 -0300

    Creamos las directivas Input y Output | Conectamos la informacion transmitida entre el componente padre y el hijo | Mostramos la informacion en un carrusel y una notificacion

--- src/app/modules/producto/components/card/card.component.ts
+++ src/app/modules/producto/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Producto } from 'src/app/models/producto';
 import { CrudService } from 'src/app/modules/admin/services/crud.service';
 
@@ -16,6 +16,12 @@ productoSeleccionado!: Producto;
 
 modalVisible: boolean= false;
 
+compraVisible: boolean=false;
+
+//directivas para comunicarse con el componente padre
+@Input() productoReciente: string = '';
+@Output() productoAgregado = new EventEmitter<Producto>();
+
 constructor(public servicioCrud: CrudService){}
 
 ngOninit(): void{
@@ -30,5 +36,11 @@ mostrarVer(info: Producto){
 this.modalVisible=true;
 //Guardo en variable seleccioanado la informacion de prodcuto elegido
 this.productoSeleccionado=info;
+}
+
+agregarProducto(info : Producto){
+this.productoAgregado.emit(info);
+
+this.compraVisible = true;
 }
 }
--- src/app/modules/producto/pages/producto/producto.component.ts
+++ src/app/modules/producto/pages/producto/producto.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Producto } from 'src/app/models/producto';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-producto',
@@ -6,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./producto.component.css']
 })
 export class ProductoComponent {
+  //string que modificara el valor de @input en el componente hijo
+product:string='';
 
+//coleccion de productos añadidos a la lista
+productosCarrusel: Producto[]=[];
+
+productoAnanido(producto:Producto){
+
+  //remplazamos el valor de product
+  this.product = `${producto.nombre} :$${producto.precio}`;
+
+try{//agregamos la informacion recibida por el parametro de la funcion
+  this.productosCarrusel.push(producto);
+
+  Swal.fire({
+    title:'God Job',
+    text:'Ha añadido el producto con exito',
+    icon:'info'
+  })
+  }catch(error){
+    Swal.fire({
+      title:'NOOOOOOOOOOOOOOOO',
+      text:'Ha ocurrido un error\n'+error,
+      icon:'info'
+    })
+  }
+}
 }
